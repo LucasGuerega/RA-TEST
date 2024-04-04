dc454db7e7f459e2b3ae00b9d6725d302db5585a
Lucas Guerega <lucas.g2006@aluno.ifsc.edu.br>
2024-04-04 16:44:26 -0300

    3

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,26 +1,12 @@
-const btnFecho = document.querySelector("#btn6");
-const btnLinkedin = document.querySelector("#btn1");
-const btnTwitter = document.querySelector("#twitter-button");
-const btnEmail = document.querySelector("#email-button");
-const btnWebsite = document.querySelector("#web-button");
+const btnInspira = document.querySelector("#btn1");
+const btnCintura= document.querySelector("#btn2");
+const btnFecho = document.querySelector("#btn3");
+const btnPence = document.querySelector("#btn4");
+const btnComprimento = document.querySelector("#btn5");
+const btnAmplitude = document.querySelector("#btn6");
+const btnBainha = document.querySelector("#btn7");
+const btnDeco = document.querySelector("#btn8");
 
 btnFecho.addEventListener('click', () => {
     window.open('https://raw.githubusercontent.com/LucasGuerega/RA-TEST/main/assets/images/material/fecho.gif', '_blank');
-});
-
-btnLinkedin.addEventListener('click', () => {
-    window.open('https://twitter.com/', '_blank');
-});
-
-btnTwitter.addEventListener('click', () => {
-    window.open('https://twitter.com/', '_blank');
-});
-
-
-btnEmail.addEventListener('click', () => {
-    window.open('mailto:labs-l@pitang.com');
-});
-
-btnWebsite.addEventListener('click', () => {
-    window.open('https://www.pitang.com/', '_blank');
 });
